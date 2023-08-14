29085212aaf54f4b3c0d6280f3d74d2ec7f7e7c9
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2023-08-14 02:53:54 +0000

    many good changes

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,5 +3,76 @@
 # "Python TDD Workflow - Unit Testing Code Example for Beginners"
 # https://www.youtube.com/watch?v=ibVSPVz2LAA
 
-def cipher(a, b, c):
-    return ""
+import string
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(module)s : %(lineno)d : %(message)s')
+file_handler = logging.FileHandler('caesar_cipher.log')
+#file_handler.setLevel(logging.INFO) #An example of changing the logging on file handler rather than the logger
+file_handler.setFormatter(formatter)
+
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+# logging.basicConfig(filename='caesar_cipher.log', level=logging.DEBUG, 
+#                    format='%(asctime)s : %(levelname)s : %(module)s : %(lineno)d : %(message)s')
+
+# logger.basicConfig(filename='caesar_cipher.log', level=logging.DEBUG, 
+#                   format='%(asctime)s : %(levelname)s : %(module)s : %(lineno)d : %(message)s')
+
+
+def engine(operation, message, offset):
+    # logging.debug(f"operation:{operation}, message: {message}, offset: {offset}")
+    logger.debug(f"operation:{operation}, message: {message}, offset: {offset}")
+    abc = string.ascii_letters + string.punctuation + string.digits + " "
+    output = "".join( [abc[abc.find(char) + 1] for index, char in enumerate(message)] )
+    logger.debug(f"output = {output}")
+    return output
+
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    try:
+        result =  x / y
+    except ZeroDivisionError:
+        logger.exception('Tried to divide by zero')
+    else:
+        return result
+
+if __name__ == '__main__':
+    num_1 = 10
+    num_2 = 0
+
+    add_result = add(num_1, num_2)
+    # logging.debug('Add: {} - {} = {}'.format(num_1, num_2, add_result))
+    logger.debug('Add: {} - {} = {}'.format(num_1, num_2, add_result))
+
+    sub_result = subtract(num_1, num_2)
+    # logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+    logger.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+
+    mul_result = multiply(num_1, num_2)
+    # logging.debug('Mul: {} - {} = {}'.format(num_1, num_2, mul_result))
+    logger.debug('Mul: {} - {} = {}'.format(num_1, num_2, mul_result))
+
+    div_result = divide(num_1, num_2)
+    # logging.debug('Div: {} - {} = {}'.format(num_1, num_2, div_result))
+    logger.debug('Div: {} - {} = {}'.format(num_1, num_2, div_result))
--- caesar_cipher_test.py
+++ caesar_cipher_test.py
@@ -1,10 +1,16 @@
+import io
+import contextlib
 import unittest
 import caesar_cipher as cipher
+import string
 
+f = None
 
 class Encryption_Test(unittest.TestCase):
     def setUp(self):
+        self.longMessage = True
         self.my_message = ""
+        f = io.StringIO()
 
     def test_inputs_should_exist(self):
         self.assertIsNotNone(self.my_message)
@@ -13,7 +19,28 @@ class Encryption_Test(unittest.TestCase):
         self.assertIsInstance(self.my_message, str)
 
     def test_cipher_should_return_something(self):
-        self.assertIsNotNone(cipher.cipher("encode", "a", 1))
+        self.assertIsNotNone(cipher.engine("encode", "a", 1))
+    
+    def test_eng_should_return_same_len_str(self):
+        term = "banana"
+        self.assertEqual(len(term), len(cipher.engine("encode", "banana", 1)))
+    
+    def test_encode_1_a_should_return_b(self):
+        expected = "b"
+        actual = cipher.engine("encode", "a", 1)
+        self.assertEqual(expected, actual)
+    
+    def test_output_should_be_string(self):
+        #expected = "b"
+        actual = cipher.engine("encode", "a", 1)
+        self.assertIsInstance(actual, str)
+
+    def test_shiftedCipher(self):
+        abc = string.ascii_letters + string.punctuation + string.digits + " "
+        actual  = abc[abc.find("c") + 1]
+        expected = "d"
+        self.assertEqual(expected, actual)
+                                       
 
 if __name__ == "__main__":
     unittest.main()
